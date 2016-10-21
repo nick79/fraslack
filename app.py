d27bc99e25282b20cb29b8164bcc8d630fb99bac
Milan Nikic <milnik79@gmail.com>
2016-10-21 21:01:40 +0200

    Added support for serving static files necessary for testing.

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import os
 from flask import Flask
 from flask import render_template
+from flask import send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('hello.html')
+
+
+@app.route('/data/<path:path>')
+def send_data(path):
+    return send_from_directory('static', path)
 
 
 if __name__ == '__main__':
