e87868de994e8f29cbe72b239a0e841c3fa26bc8
Milan Nikic <milnik79@gmail.com>
2016-10-22 17:55:42 +0200

    First version of slack handler.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 from flask import Flask
+from flask import Response
 from flask import render_template
+from flask import request
 from flask import send_from_directory
+
+SLACK_TOKEN = 'LxqD74GgYpSCmQlNsl5MXckS'
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -14,6 +18,17 @@ def index():
 @app.route('/data/<path:path>')
 def send_data(path):
     return send_from_directory('static', path)
+
+
+@app.route('/slack', methods=['POST'])
+def slack():
+    if request.form.get('token') == SLACK_TOKEN:
+        channel = request.form.get('channel_name')
+        username = request.form.get('user_name')
+        text = request.form.get('text')
+        inbound_message = username + " in " + channel + " says: " + text
+        print(inbound_message)
+    return Response('Milan Test'), 200
 
 
 if __name__ == '__main__':
